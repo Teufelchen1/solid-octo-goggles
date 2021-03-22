04f88b6a4db68fb1bd7bd3cf2eccb6cb5c9acb0f
teufelchen1 <bennet.blischke@outlook.com>
2021-03-22 14:43:18 +0100

    Updating Scheune

--- tiled_maps/Tilesets/Tilesets/ak_#039_s_assets.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="ak&amp;#039;s_assets" tilewidth="32" tileheight="32" tilecount="504" columns="24">
- <image source="ak_#039_s_assets.png" width="768" height="672"/>
-</tileset>
